1ca5a718b84a0123ee9577604bd519884d3b7606
NicoPaez <nicopaez@gmail.com>
2015-01-11 13:50:28 -0300

    Nginx support added

--- manifests/runtime.pp
+++ manifests/runtime.pp
@@ -3,5 +3,15 @@ class jobvacancy::runtime {
   class { 'java':
     distribution => 'jre',
   }
+
+  class { 'nginx': }
   
+  nginx::resource::upstream { 'jobvacancy_app':
+    members => ['localhost:8080'],
+  }
+
+  nginx::resource::vhost { 'jobvacancy.com':
+    proxy => 'http://jobvacancy_app',
+  }
+
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,16 @@
 class jobvacancy::service {
-  
+
   user { 'jobvacancy':
     ensure => 'present',
   }
-  
+
+  file { ['/var/www/', '/var/www/jobvacancy']:
+    ensure => 'directory',
+  }
+
   wget::fetch { "download-jar":
     source      => 'https://nicopaez.ci.cloudbees.com/view/ejemplos/job/JobVacancy-release/lastSuccessfulBuild/artifact/jobvacancy.jar',
-    destination => '/var/www/jobvacancy.jar',
+    destination => '/var/www/jobvacancy/jobvacancy.jar',
     timeout     => 0,
     verbose     => true,
     before => Service['jobvacancy']
@@ -16,10 +20,10 @@ class jobvacancy::service {
     ensure => present,
     content => template("jobvacancy/jobvacancy.conf.erb")
   }
-  
+
   service { 'jobvacancy':
     ensure => running,
     provider => 'upstart',
   }
-  
+
 }
